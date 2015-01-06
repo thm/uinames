a505474d078e49563d6f4a6d6a2b354db867d534
thom <thomweerd@gmail.com>
2015-01-06 12:46:18 +0100

    Added Front-end

---
+++ assets/js/site.js
@@ -0,0 +1,211 @@
+function addListener(li, ev, fn) {
+    var ev = ev.split(' ');
+    
+    for (var i = 0, l = ev.length; i < l; i++) {
+        if (li && window.addEventListener) {
+            li.addEventListener(ev[i], fn, false);
+        } else if (li && window.attachEvent) {
+            li.attachEvent('on' + ev[i], fn);
+        }
+    }
+}
+
+function hasClass(elem, klass) {
+     return (' ' + elem.className + ' ').indexOf(' ' + klass + ' ') > -1;
+}
+
+(function() {
+	
+	var body = document.getElementsByTagName('body')[0],
+		h1 = document.getElementsByTagName('h1')[0],
+		genders = document.getElementById('genderSelect').getElementsByTagName('a'),
+		infoToggle = document.getElementsByClassName('info')[0],
+		countrySelect = document.getElementsByClassName('icon country')[0],
+		countryBox = document.getElementById('country'),
+		availableCountries = countryBox.getElementsByTagName('li'),
+		infoBox = document.getElementById('info');
+	
+	// NAME RANDOMIZER
+	function randomizer() {
+		if (req.readyState == 4) {
+			var data = eval('(' + req.responseText + ')');
+			
+			function countryToggle() {
+				for (var j = 0; j < availableCountries.length; j++) {
+					availableCountries[j].className = availableCountries[j].className.replace(/\b ?active\b/g, '');
+				}
+				this.className += ' active';
+				
+				countrySelect.className = countrySelect.className.replace(/\b ?active\b/g, '');
+				localStorage.setItem('country', this.innerHTML);
+				
+				setTimeout(function() {
+					body.removeAttribute('data-popup');
+				}, 300);
+			}
+			
+			for (var i = 0; i < availableCountries.length; i++) {
+				addListener(availableCountries[i], 'click', countryToggle);
+			}
+			
+			// select country from local storage if saved
+			var storedcountry = localStorage.getItem('country');
+			if (storedcountry != null) {
+				for (var i = 1, n = availableCountries.length; i < n; i++) {
+					if (availableCountries[i].innerHTML == storedcountry) {
+						availableCountries[i].click();
+						break;
+					}
+				}
+			}
+						
+			function getName(e) {
+				if (e.keyCode == 32 || e.which == 32 || e.type == 'touchend') {
+					
+					var country = countryBox.getElementsByClassName('active')[0].className.replace(/[^0-9]/g, '') - 1,
+						gender = genders[0].parentNode.getElementsByClassName('active')[0].hash.substr(1);
+					
+					if (country < 0) {
+						country = Math.floor(Math.random() * data.length);
+					}
+					
+					if (gender == 'random') {
+						if (Math.floor(Math.random() * 2) == 0) {
+							gender = 'male';
+						} else {
+							gender = 'female';
+						}
+					}
+					
+					// generating html
+					var first = data[country][gender][Math.floor(Math.random() * data[country][gender].length)],
+						last = data[country]['surnames'][Math.floor(Math.random() * data[country]['surnames'].length)];
+					
+					// russian exceptions
+					if (gender == 'female' && data[country]['country'].toLowerCase() == 'russia' && /[в|н]/.test(last[last.length - 1])) {
+						 last += 'a';
+					}
+					
+					h1.innerHTML = first + ' ' + last;
+					h1.style.display = '';
+					
+					var specs = document.getElementById('specs'),
+						help = document.getElementById('help');
+					
+					specs.innerHTML = '<span>' + gender.charAt(0).toUpperCase() + gender.slice(1) + '</span> from <span>' + data[country]['country'] + '</span><a class="bg-check" href="http://duckduckgo.com/?q=' + first + '+' + last + '" target="_blank">Background Check</a>';
+					
+					body.removeAttribute('data-popup');
+					
+					infoToggle.className = infoToggle.className.replace(/\b ?active\b/g, '');
+					countrySelect.className = countrySelect.className.replace(/\b ?active\b/g, '');
+					
+					specs.style.display = '';
+					help.style.display = '';
+					help.className += ' animate';
+					
+					setTimeout(function() {
+						help.className = 'help';
+					}, 250);
+					
+				}
+			}
+			
+			addListener(document, 'keyup', getName);
+			addListener(h1, 'touchend', getName);
+			
+			function select(e) {
+				if (e.keyCode == 67 || e.which == 67) {
+				
+					if (!hasClass(body, 'touch-device') && !h1.getElementsByTagName('em')[0]) {
+						var range, selection;
+						if (document.body.createTextRange) {
+							range = document.body.createTextRange();
+							range.moveToElementText(h1);
+							range.select();
+						} else if (window.getSelection) {
+							selection = window.getSelection();
+							range = document.createRange();
+							range.selectNodeContents(h1);
+							selection.removeAllRanges();
+							selection.addRange(range);
+						}
+					}
+				
+				}
+			}
+			
+			addListener(document, 'keyup', select);
+		}
+	}
+	
+	var req = new XMLHttpRequest();
+	req.open("GET", "./api/names.json", true);
+	req.onreadystatechange = randomizer;
+	req.send(null);
+	
+	// GENDER TOGGLE
+	function toggleGender(e) {
+	    e.preventDefault();
+	    e.stopPropagation();
+	
+        if (!this.className.match(/active/)) {
+            for (var i = 0; i < genders.length; i++) {
+                genders[i].className = genders[i].className.replace(/\b ?active\b/g, '');
+            }
+            this.className += ' active';
+        }
+    }
+    
+    for (var i = 0; i < genders.length; i++) {
+    	addListener(genders[i], 'click', toggleGender);
+    }
+	
+	// POPUP TOGGLE
+	function togglePopup(e) {
+	    e.preventDefault();
+	    e.stopPropagation();
+	    
+	    var hash = this.hash.substr(1);
+		
+		if (body.getAttribute('data-popup') == hash) {
+			body.removeAttribute('data-popup');
+		} else {
+			body.setAttribute('data-popup', hash);
+		}
+	}
+	
+	addListener(infoToggle, 'click', togglePopup);
+	addListener(countrySelect, 'click', togglePopup);
+	
+	// INFO TABS TOGGLE
+	var tabs = document.getElementById('tabs').getElementsByTagName('a');
+	
+	function toggleTab(e) {
+	    e.preventDefault();
+	    e.stopPropagation();
+	
+        if (!this.className.match(/active/)) {
+            for (var i = 0; i < tabs.length; i++) {
+                tabs[i].className = tabs[i].className.replace(/\b ?active\b/g, '');
+            }
+            this.className += ' active';
+            infoBox.setAttribute('data-tab', this.hash.substr(1));
+        }
+    }
+    
+    for (var i = 0; i < tabs.length; i++) {
+    	addListener(tabs[i], 'click', toggleTab);
+    }
+	
+})();
+	
+// GOOGLE'S STALKING CODE
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-46677803-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
---
+++ index.php
@@ -0,0 +1,186 @@
+<?php
+	
+	$data = file_get_contents('api/names.json');
+	$names = json_decode($data, true);
+	
+	$data = file_get_contents('api/stats.json');
+	$stats = json_decode($data, true);
+	
+	$countries = $stats['countries'];
+	$available = $stats['names'];
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+
+	<meta charset="utf-8" />
+	
+	<title>uinames.com</title>
+	<meta name="description" content="A simple tool to generate random names for use in designs and mockups." />
+	
+	<link rel="shortcut icon" href="assets/img/favicon.ico" />
+	<script>
+		var isRetina = (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia("(-webkit-min-device-pixel-ratio: 1.5),(-moz-min-device-pixel-ratio: 1.5),(min-device-pixel-ratio: 1.5)").matches)); if (isRetina) document.write('<link rel="shortcut icon" href="assets/img/favicon@2x.ico" />');
+	</script>
+	
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	
+	<link href="assets/css/styles.css" rel="stylesheet" />
+	
+	<!-- viewport -->
+	<meta name="viewport" content="user-scalable=no, initial-scale=1" />
+	
+	<!-- identity -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-title" content="uinames">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon" href="assets/img/ios-precomposed.png" />
+
+</head>
+
+<body class="no-js">
+
+	<script>
+		var body = document.getElementsByTagName('body')[0],
+			action = 'Press Spacebar', c = 'ctrl+c';
+		
+		body.className = body.className.replace(/no\-js/, '');
+		
+		if ('ontouchstart' in window || 'onmsgesturechange' in window) {
+			body.className += ' touch-device';
+			action = 'Tap here';
+		}
+		
+		if (navigator.appVersion.indexOf('Mac')!=-1) {
+			c = 'cmd+c';
+		}
+	</script>
+
+	<h1><em><script>document.write(action)</script></em></h1>
+	<p id="specs" style="display: none"></p>
+	<p id="help" style="display: none">press <strong>c</strong> to select, then <strong><script>document.write(c)</script></strong> to copy</p>
+	
+	<div id="info" class="clearfix" data-tab="info">
+		<div id="tabs">
+			<a href="#info" class="active">Info</a>
+			<a href="#api">API</a>
+		</div>
+		<div id="tab-info">
+			<h2>About</h2>
+			<p><a href="http://uinames.com">uinames.com</a> is a simple tool to generate names for use in designs and mockups. Made by <a href="http://twitter.com/thomweerd" target="_blank">Thom</a>.</p>
+			<h2>Elsewhere</h2>
+			<p>Numerous websites and blogs have published about the project including <a href="http://tympanus.net/codrops/collective/collective-96/" target="_blank">Codrops</a>, <a href="http://speckyboy.com/2014/01/30/40-tiny-web-based-apps-tools-web-designers/" target="_blank">Speckyboy</a>, <a href="http://www.smashingmagazine.com/smashing-newsletter-issue-new-year-special-edition-2013/" target="_blank">Smashing Magazine</a>, <a href="http://sidebar.io/2013/12/26" target="_blank">Sidebar</a>, <a href="http://www.webdesignerdepot.com/2014/02/whats-new-for-designers-february-2014/" target="_blank">Webdesigner Depot</a>, <a href="http://www.cssauthor.com/weekly-web-development-resources-tools-35/" target="_blank">CSS Author</a>, <a href="http://www.producthunt.co/posts/1906" target="_blank">Product Hunt</a>, <a href="https://news.layervault.com/stories/12511-ui-names" target="_blank">LayerVault</a> and <a href="http://oozled.com/resources/just-handy" target="_blank">Oozled</a>.</p>
+			<h2>Contribute</h2>
+			<p>You can contribute to the project through <a href="http://github.com/thm/uinames" target="_blank">Github</a>, and feedback can be tweeted to <a href="http://twitter.com/thomweerd" target="_blank">@thomweerd</a> directly.</p>
+			<h2>Other</h2>
+			<p>Check out <a href="http://uifaces.com" target="_blank">uifaces.com</a> as well!</p>
+		</div>
+		<div id="tab-api">
+			<h2>Overview</h2>
+			<p>All responses are returned as JSON, and there is no request limit. Please keep the amount of calls to a minimum though, and cache responses whenever possible.</p>
+			<h2>Usage</h2>
+			<p>Basic usage:</p>
+<pre>http://api.uinames.com
+---
+{
+  "name":"John",
+  "surname":"Doe",
+  "gender":"male",
+  "country":"United States"
+}
+</pre>
+			<h2>Optional Parameters</h2>
+			<p>The amount of names to return:</p>
+			<pre><span>http://api.uinames.com/</span>?amount=25</pre>
+			<br />
+			<p>The gender of names to return (male or female):</p>
+			<pre><span>http://api.uinames.com/</span>?gender=female</pre>
+			<br />
+			<p>Country specific results:</p>
+			<pre><span>http://api.uinames.com/</span>?country=germany</pre>
+			<!--
+			<br />
+			<p>Results for a language within a country:</p>
+			<pre><span>http://api.uinames.com/</span>?country=switzerland&amp;language=german</pre>
+			-->
+		</div>
+		<div id="facts">
+			<div><?php print $countries; ?><p>Countries</p></div>
+			<div><?php print number_format($available, 0, ',', '.'); ?><p>Available Names</p></div>
+		</div>
+	</div>
+	
+	<div id="country" class="clearfix">
+		<ul>
+			<li class="pos-0">Randomize</li>
+			<?php
+				
+				$newCountries = array("Japan");
+				$favCountries = array("United States");
+				
+				$total = count($names);
+			
+				for ($i = 0; $i < $total; $i++) {
+					$country = $names[$i]['country'];
+					
+					$new = '';
+					if (in_array($country, $newCountries)) {
+						$new = ' new';
+					}
+					
+					$fav = '';
+					if (in_array($country, $favCountries)) {
+						$fav = '';
+						if ($country == $favCountries[0]) {
+							$fav = ' active';
+						}
+					}
+					
+					$oddLastFix = '';
+					if (($i + 1) == $total && $total % 2 == 0) {
+						$oddLastFix = ' oddLastFix';
+					}
+					
+					echo '<li class="pos-' . ($i + 1) . $new . $fav . $oddLastFix . '">' . $country . '</li>';
+					
+					if ($i == floor((count($names)/2) - 1)) {
+						echo '</ul><ul>';
+					}
+				}
+			
+			?>
+		</ul>
+	</div>
+	
+	<div id="options">
+		<span id="genderSelect">
+			<a class="icon random active" href="#random" title="Random Gender"><span class="r1"></span><span class="r2"></span><span class="r3"></span><span class="r4"></span><span class="r5"></span><span class="r6"></span><span class="r7"></span></a>
+			<a class="icon male" href="#male" title="Male Only"><span class="m1"></span><span class="m2"></span><span class="m3"></span><span class="m4"></span></a>
+			<a class="icon female" href="#female" title="Female Only"><span class="f1"></span><span class="f2"></span><span class="f3"></span></a>
+		</span>
+		<span id="countrySelect">
+			<a class="icon country" href="#country" title="Select Country"><span class="r1"></span><span class="r2"></span><span class="r3"></span><span class="r4"></span></a>
+		</span>
+	</div>
+	
+	<div id="details">
+		<a class="icon info" href="#info" title="More Information"><span class="i1"></span><span class="i2"></span><span class="i3"></span></a>
+	</div>
+	
+	<div id="share-box">
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://uinames.com" data-text="Generate random names for use in designs and mockups on" data-via="thomweerd" data-count="horizontal" data-size="small" data-related="thomweerd" data-dnt="true">Tweet</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		
+		<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fuinames.com&amp;width=100px&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=65" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:20px;" allowTransparency="true"></iframe>
+	
+		<iframe src="http://ghbtns.com/github-btn.html?user=thm&repo=uinames&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="84" height="20"></iframe>
+	</div>
+	
+	<script src="assets/js/site.js"></script>
+	<script src="assets/js/fastclick.min.js"></script>
+
+</body>
+</html>
